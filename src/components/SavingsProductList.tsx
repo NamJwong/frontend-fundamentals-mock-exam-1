1e6e548c2e033ed5f766a20f0933e0d632dff1db
jynam <njy1007@gmail.com>
2025-11-23 22:15:15 +0900

    refactor: SavingsProduct 컴포넌트 분리, feat: 적금 상품 호출 API 구현

---
+++ src/components/SavingsProductList.tsx
@@ -0,0 +1,69 @@
+import { useQuery } from '@tanstack/react-query';
+import { getSavingsProducts } from 'services';
+import { Assets, colors, ListRow } from 'tosslib';
+import { SavingsProduct } from 'types';
+
+type Props = {
+  savings: SavingsProduct[];
+  onSavingClick: () => void;
+};
+
+export default function SavingsProductList({ savings }: Props) {
+  const savingsProductsQuery = useQuery({ queryKey: ['savingsProducts'], queryFn: getSavingsProducts });
+
+  if (savingsProductsQuery.isLoading || savingsProductsQuery.data === undefined) {
+    return <div>적금 상품을 불러오는 중입니다.</div>;
+  }
+
+  return savings.map(({ id, name, annualRate, minMonthlyAmount, maxMonthlyAmount, availableTerms }) => (
+    <ListRow
+      key={id}
+      contents={
+        <ListRow.Texts
+          type="3RowTypeA"
+          top={name}
+          topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
+          middle={`연 이자율: ${annualRate}%`}
+          middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
+          bottom={`${minMonthlyAmount}원 ~ ${maxMonthlyAmount}원 | ${availableTerms}개월`}
+          bottomProps={{ fontSize: 13, color: colors.grey600 }}
+        />
+      }
+      right={<Assets.Icon name="icon-check-circle-green" />}
+      onClick={() => {}}
+    />
+  ));
+  return (
+    <>
+      <ListRow
+        contents={
+          <ListRow.Texts
+            type="3RowTypeA"
+            top={'기본 정기적금'}
+            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
+            middle={'연 이자율: 3.2%'}
+            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
+            bottom={'100,000원 ~ 500,000원 | 12개월'}
+            bottomProps={{ fontSize: 13, color: colors.grey600 }}
+          />
+        }
+        right={<Assets.Icon name="icon-check-circle-green" />}
+        onClick={() => {}}
+      />
+      <ListRow
+        contents={
+          <ListRow.Texts
+            type="3RowTypeA"
+            top={'고급 정기적금'}
+            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
+            middle={'연 이자율: 2.8%'}
+            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
+            bottom={'50,000원 ~ 1,000,000원 | 24개월'}
+            bottomProps={{ fontSize: 13, color: colors.grey600 }}
+          />
+        }
+        onClick={() => {}}
+      />
+    </>
+  );
+}
